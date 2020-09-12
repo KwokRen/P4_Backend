675ef9d02d3333d3205204cdcf52ea3785a98f0d
Kwok <kwokrenx@gmail.com>
2020-09-12 15:23:34 -0400

    adding task model

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.authentication.models import User
 
 # Create your models here.
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
