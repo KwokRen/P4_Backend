2c752b4d4387d67fe55bf86f1e23a0e1e351b8e6
Kwok <kwokrenx@gmail.com>
2020-09-12 15:39:20 -0400

    finished item model

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 from apps.authentication.models import User
 
-# Create your models here.
-
 
 class Task(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'tasks'
+
+    user = models.ForeignKey(User, related_name="tasks", on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
     date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'items'
+
+    user = models.ForeignKey(User, related_name="items", on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, related_name="items", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
